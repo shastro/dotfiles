6d69265dbc6d750d6d2abee0cbb4eb84ce534f4e
shastro <guitarshughes@hotmail.com>
2022-01-19 06:58:18 -0700

    Update

--- startscript.sh
+++ startscript.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-feh --bg-scale /home/shastro/Pictures/mountain.jpg
+feh --bg-center /home/shastro/Pictures/green.jpg
 picom 2>/dev/null &
 /home/shastro/dwm-bar/dwm_bar.sh &>/dev/null &
