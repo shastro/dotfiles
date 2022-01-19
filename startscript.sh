f86337ec3270df64878398b1b51083e0f71fba83
shastro <guitarshughes@hotmail.com>
2022-01-19 04:34:37 -0700

    Updates

--- startscript.sh
+++ startscript.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-feh --bg-center /home/shastro/Pictures/PIA12800.tif
+feh --bg-scale /home/shastro/Pictures/mountain.jpg
 picom 2>/dev/null &
 /home/shastro/dwm-bar/dwm_bar.sh &>/dev/null &
